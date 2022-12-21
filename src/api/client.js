27f87c1c24904bb91f5522fa8c01ea0e17af0478
RACHT16 <rachit.ar16@gmail.com>
2022-12-21 01:13:28 -0800

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://movie-review-backend.vercel.app/api",
 });
 
 export default client;
